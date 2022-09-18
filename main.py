de3b1db5811ced45f5c9f95592126179d6b50d7b
Nikom22 <jnmolinav@misena.edu.co>
2022-09-18 11:07:53 -0500

    Create main.py
    practica ejercicio python

---
+++ main.py
@@ -0,0 +1,21 @@
+# Ejemplos Variables
+"""
+mensaje = "Hola mundo estoy aprendiendo python"
+print(mensaje)
+"""
+
+nombre = "Nicolas"
+apellido = "Molina"
+telefono = 3232214128
+ciudad = "Bogota"
+"""
+# print(nombre + " " + apellido + " " + ciudad + " " + str(telefono))
+# print(nombre,apellido,ciudad,telefono)
+print("hola %s de %s" % (nombre, ciudad))
+print(f"Hola {nombre} {apellido} de la ciudad {ciudad} con telefono {telefono}")
+"""
+
+# Leer datos desde el teclado
+nombre = input("digita tu nombre: ")
+print(nombre)
+
