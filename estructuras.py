dde25c67b15bbe46dcf9d05cb17c3148dc8c8c27
Nikom22 <jnmolinav@misena.edu.co>
2022-09-19 21:46:10 -0500

    Create estructuras.py

---
+++ estructuras.py
@@ -0,0 +1,16 @@
+# El comando IF
+# num= int(input("Ingrese un numero:\n"))
+
+# if num ==100:
+#     print("Usted escribio el 100")
+# elif num > 100:
+#     print("Usted escribio un numero mayor a 100")
+# else:
+#     print("Usted escribio el numero menor a 100")
+
+# Condicionales multiples
+
+x=5
+if 0 < x:
+    if x < 10:
+        print("x es un numero positivo de un solo digito")
